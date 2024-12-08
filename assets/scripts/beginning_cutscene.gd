0942e290af22bc1beee369692813dc67b965b22c
ofcourseitsgv <84610338+ofcourseitsgv@users.noreply.github.com>
2024-12-08 14:39:26 -0800

    multiple lines of dialogue

--- assets/scripts/beginning_cutscene.gd
+++ assets/scripts/beginning_cutscene.gd
@@ -1,17 +1,35 @@
 extends Node2D
 
+var dialogue_0 = [
+	"Ever since I was little, I've always wanted to design outfits for others.",
+	"So, when my dad built this shop for me, I knew it was finally time for me to chase my dreams!",
+	"But if I get famous, I won't be able to live a normal life anymore...",
+	"I guess I'll just change my appearance on the job!",
+]
+
+var current_line: int
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$CanvasLayer/DialogueText.visible_characters = 0
+	current_line = 0
+	$CanvasLayer/DialogueText.text = dialogue_0[current_line]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	if $CanvasLayer/DialogueText.visible_characters < $CanvasLayer/DialogueText.get_total_character_count():
 		$CanvasLayer/DialogueText.visible_characters += 1
-	if Input.is_action_just_pressed("dialogue_skip"):
+	
+	if Input.is_action_just_pressed("dialogue_key"):
 		if $CanvasLayer/DialogueText.visible_characters >= $CanvasLayer/DialogueText.get_total_character_count():
 			$CanvasLayer/DialogueText.visible_characters = 0
+			current_line += 1
+			if current_line < dialogue_0.size():
+				$CanvasLayer/DialogueText.text = dialogue_0[current_line]
+			else:
+				print("finished dialogue_0")
 		else:
 			$CanvasLayer/DialogueText.visible_characters = $CanvasLayer/DialogueText.get_total_character_count()
