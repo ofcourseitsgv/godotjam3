2cf91389f94a1dc8e888c251f0809522b68b8b71
ofcourseitsgv <ygenji4@gmail.com>
2024-12-08 13:26:12 -0800

    add font, start scene structures

---
+++ assets/scripts/main_menu.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# FIXME: figure out moving BG based on mouse position
+#func _input(event: InputEvent) -> void:
+	#if event is InputEventMouseMotion:
+		#print("mouse:", event.position)
+		#$Camera2D.position = -event.position
+		#print("camera:", $Camera2D.position)
+		
