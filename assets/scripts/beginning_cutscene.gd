767ebc3a2ddfc7c65771d378eb8c1553f79c2245
ofcourseitsgv <84610338+ofcourseitsgv@users.noreply.github.com>
2024-12-08 14:54:51 -0800

    floating logo

--- assets/scripts/beginning_cutscene.gd
+++ assets/scripts/beginning_cutscene.gd
@@ -6,6 +6,7 @@ var dialogue_0 = [
 	"But if I get famous, I won't be able to live a normal life anymore...",
 	"I guess I'll just change my appearance on the job!",
 ]
+var dialogue_0_flag = false
 
 var current_line: int
 
@@ -23,13 +24,17 @@ func _process(delta: float) -> void:
 	if $CanvasLayer/DialogueText.visible_characters < $CanvasLayer/DialogueText.get_total_character_count():
 		$CanvasLayer/DialogueText.visible_characters += 1
 	
-	if Input.is_action_just_pressed("dialogue_key"):
+	if Input.is_action_just_pressed("dialogue_key") and not dialogue_0_flag:
 		if $CanvasLayer/DialogueText.visible_characters >= $CanvasLayer/DialogueText.get_total_character_count():
-			$CanvasLayer/DialogueText.visible_characters = 0
 			current_line += 1
 			if current_line < dialogue_0.size():
+				$CanvasLayer/DialogueText.visible_characters = 0
 				$CanvasLayer/DialogueText.text = dialogue_0[current_line]
 			else:
 				print("finished dialogue_0")
+				dialogue_0_flag = true
+				$CanvasLayer.visible = false
 		else:
 			$CanvasLayer/DialogueText.visible_characters = $CanvasLayer/DialogueText.get_total_character_count()
+	
+	
--- assets/scripts/main_menu.gd
+++ assets/scripts/main_menu.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
+var logo_start_pos
+var amplitude := 50.0
+var frequency := 4.5
+var time := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	logo_start_pos = $Logo.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	time += delta * frequency
+	$Logo.position = logo_start_pos + Vector2(0, sin(time) * amplitude)
+	print($Logo.position)
 
 # FIXME: figure out moving BG based on mouse position
 #func _input(event: InputEvent) -> void:
