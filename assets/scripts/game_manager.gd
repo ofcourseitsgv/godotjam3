7d04b056108c72fec53752d4385fffc84e57ca82
ofcourseitsgv <ygenji4@gmail.com>
2024-12-09 10:07:36 -0800

    hub canvas

--- assets/scripts/game_manager.gd
+++ assets/scripts/game_manager.gd
@@ -4,8 +4,27 @@ extends Node2D
 func _ready() -> void:
 	print(Player.player_name + ": $" + str(Player.wallet) + ", " + str(Player.reputation) + " rep")
 	print(Shop.shop_name)
+	
+	$HubCanvas/NameEditors/PlayerNameEdit.text = Player.player_name
+	$HubCanvas/NameEditors/ShopNameEdit.text = Shop.shop_name
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_player_name_confirm_pressed() -> void:
+	var new_name = $HubCanvas/NameEditors/PlayerNameEdit.text
+	if (new_name == ""):
+		$HubCanvas/NameEditors/PlayerNameEdit.text = Player.player_name
+		return
+	Player.player_name = new_name
+
+
+func _on_shop_name_confirm_pressed() -> void:
+	var new_name = $HubCanvas/NameEditors/ShopNameEdit.text
+	if (new_name == ""):
+		$HubCanvas/NameEditors/ShopNameEdit.text = Player.player_name
+		return
+	Shop.shop_name = new_name
--- assets/scripts/shop.gd
+++ assets/scripts/shop.gd
@@ -4,4 +4,4 @@ static var shop_name: String
 static var upgrades = []
 
 func _init() -> void:
-	shop_name = "Shop"
+	shop_name = "My Own Shop"
